f97fad3e206c160855aa169c24d30bf092f9873b
randyren278 <randyren278@gmail.com>
2024-07-26 12:45:37 -0600

    inital information, introduction and animation

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import Image from "next/image";
+import NameType from "./components/NameType";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Potfolio Website
+    <main className="flex min-h-screen flex-col bg-[#121212]">
+    <div className="mt-24 container mx-auto py-4 px-12">
+      <NameType/>
+    </div>
     </main>
   );
 }
