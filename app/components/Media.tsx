a5cc550008a52c8d0661d9e599b1bcfd559fdac5
randyren278 <randyren278@gmail.com>
2024-07-27 00:45:18 -0600

    media buttosn hover

--- app/components/Media.tsx
+++ app/components/Media.tsx
@@ -4,27 +4,25 @@ import { TbMail } from "react-icons/tb";
 
 const Media = () => {
   return (
-    <>
+    <div className="flex space-x-2">
       <a href="mailto:randyren278@gmail.com">
-        <TbMail
-          className="text-white w-10 h-10 mx-1 hover:cursor-pointer hover:text-blue-600"
-        />
+        <TbMail className="text-white w-10 h-10 mx-1 cursor-pointer transition-transform duration-200 transform hover:scale-110 hover:text-white hover:bg-clip-text hover:border hover:border-blue-600 hover:rounded-full" />
       </a>
       <a
         href="https://www.linkedin.com/in/randyren278/"
         target="_blank"
         rel="noopener noreferrer"
       >
-        <BiLogoLinkedin className="text-white w-10 h-10 mx-1 hover:cursor-pointer hover:text-blue-600" />
+        <BiLogoLinkedin className="text-white w-10 h-10 mx-1 cursor-pointer transition-transform duration-200 transform hover:scale-110 hover:text-white hover:bg-clip-text hover:border hover:border-blue-600 hover:rounded-full" />
       </a>
       <a
         href="https://github.com/randyren278"
         target="_blank"
         rel="noopener noreferrer"
       >
-        <BiLogoGithub className="text-white w-10 h-10 mx-1 hover:cursor-pointer hover:text-blue-600" />
+        <BiLogoGithub className="text-white w-10 h-10 mx-1 cursor-pointer transition-transform duration-200 transform hover:scale-110 hover:text-white hover:bg-clip-text hover:border hover:border-blue-600 hover:rounded-full" />
       </a>
-    </>
+    </div>
   );
 };
 
