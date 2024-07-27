783f9adb226fed430ed18113bd4db35648a492f8
randyren278 <randyren278@gmail.com>
2024-07-26 20:37:39 -0600

    changed some hero section attributes

--- app/components/NameType.tsx
+++ app/components/NameType.tsx
@@ -1,25 +1,21 @@
 "use client";
 import React from "react";
-import Image from "next/image";
 import { TypeAnimation } from "react-type-animation";
 import { motion } from "framer-motion";
 import Link from "next/link";
 
 const NameType = () => {
   return (
-    <section className="lg:py-16">
-      <div className="grid grid-cols-1 sm:grid-cols-12">
+    <section className="lg:py-16 flex justify-center items-center">
+      <div className="grid grid-cols-1 text-center">
         <motion.div
           initial={{ opacity: 0, scale: 0.5 }}
           animate={{ opacity: 1, scale: 1 }}
           transition={{ duration: 0.5 }}
-          className="col-span-8 place-self-center text-center sm:text-left justify-self-start"
+          className="place-self-center"
         >
           <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-7xl lg:leading-normal font-extrabold">
-            <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-600 to-blue-800 ">
-              I&apos;m{" "}
-            </span>
-            <br></br>
+            <span className="text-white">I'm{" "}</span>
             <TypeAnimation
               sequence={[
                 "Randy",
@@ -28,18 +24,19 @@ const NameType = () => {
                 1000,
                 "a full stack developer",
                 1000,
-                "a track athlete ",
+                "a track athlete",
                 1000,
               ]}
               wrapper="span"
               speed={50}
               repeat={Infinity}
+              className="text-transparent bg-clip-text bg-gradient-to-r from-blue-600 to-blue-800"
             />
           </h1>
           <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
             <span className="block">UBC Engineering 27'</span>
             Hi, I'm Randy! I am an electrical engineering student at UBC and a
-            software developer that loves to inovate!
+            software developer that loves to innovate!
           </p>
           <div>
             <Link
@@ -56,22 +53,6 @@ const NameType = () => {
                 Download CV
               </span>
             </Link>
-          </div>
-        </motion.div>
-        <motion.div
-          initial={{ opacity: 0, scale: 0.5 }}
-          animate={{ opacity: 1, scale: 1 }}
-          transition={{ duration: 0.5 }}
-          className="col-span-4 place-self-center mt-4 lg:mt-0"
-        >
-          <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative overflow-hidden">
-            <Image
-              src="/images/randysail.png"
-              alt="randy sail"
-              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-              width={3000}
-              height={3000}
-            />
           </div>
         </motion.div>
       </div>
