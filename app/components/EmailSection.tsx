4949ac9d42b868498b03182f52b81984f05a5c6d
randyren278 <randyren278@gmail.com>
2024-07-27 18:09:47 -0600

    email section color discrepency

--- app/components/EmailSection.tsx
+++ app/components/EmailSection.tsx
@@ -53,7 +53,7 @@ const EmailSection = () => {
       <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-primary-900 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-4 transform -translate-x-1/2 -translate-1/2"></div>
       <div className="z-10">
         <h5 className="text-xl font-bold text-white my-2">
-          Let&apos;s Connect
+          Let&apos;s Get in Touch
         </h5>
         <p className="text-[#ADB7BE] mb-4 max-w-md">
           I&apos;m always looking for new opportunities, my inbox is always
@@ -116,7 +116,7 @@ const EmailSection = () => {
             </div>
             <button
               type="submit"
-              className="bg-primary-500 hover:bg-primary-600 text-white font-medium py-2.5 px-5 rounded-lg w-full"
+              className="bg-gradient-to-br from-blue-800 to-purple-600 hover:bg-gradient-to-br hover:from-purple-600 hover:to-blue-800 text-white font-medium py-2.5 px-5 rounded-lg w-full"
             >
               Send Message
             </button>
