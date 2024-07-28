897f8efc4e0e79a474aa33431adeeb79a9d567c3
randyren278 <randyren278@gmail.com>
2024-07-28 12:43:12 -0600

    apple carousel loading

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -1,110 +1,135 @@
 "use client";
-import React, { useTransition, useState } from "react";
-import TabButton from "./TabButton";
-import Skill from "./Skill"; // Import the Skill component
-import Media from "./Media"; // Import Media component
+import React from "react";
+import { Carousel, Card } from "./apple-cards-carousel"; // Adjust the import path if necessary
+import Media from "./Media";
+import Image from "next/image";
+import Skill from "./Skill";
+import { ReactNode } from "react";
 
-const TAB_DATA = [
+interface DummyContentProps {
+  children: ReactNode;
+}
+
+const DummyContent: React.FC<DummyContentProps> = ({ children }) => {
+  return (
+    <div className="bg-[#F5F5F7] dark:bg-neutral-800 p-8 md:p-14 rounded-3xl mb-4">
+      {children}
+    </div>
+  );
+};
+
+const data = [
   {
-    title: "Skills",
-    id: "skills",
+    category: "Introduction",
+    title: "Media",
+    src: "/images/carousel1.png", // Path to your image
     content: (
-      <div className="flex flex-wrap justify-center">
-        <Skill imageURL="/images/java.png" text="Java" />
-        <Skill imageURL="/images/python.png" text="Python" />
-        <Skill imageURL="/images/cpp.png" text="C++" />
-        <Skill imageURL="/images/js.png" text="JavaScript" />
-        <Skill imageURL="/images/ts.png" text="TypeScript" />
-        <Skill imageURL="/images/SQL.png" text="SQL" />
-        <Skill imageURL="/images/html.png" text="HTML5" />
-        <Skill imageURL="/images/css.png" text="CSS3" />
+      <div className="text-center">
+        <div className="flex flex-col items-center">
+          <Image
+            src="/images/myself.png"
+            alt="Image of myself"
+            width={300}
+            height={300}
+            className="rounded-full"
+          />
+          <div className="flex mt-5">
+            <Media />
+          </div>
+        </div>
       </div>
     ),
   },
   {
+    category: "Introduction",
+    title: "About Me",
+    src: "/images/carousel2.png", // Placeholder image, replace with your desired image
+    content: (
+      <DummyContent>
+        <p className="text-neutral-600 dark:text-neutral-400 text-base md:text-xl font-sans max-w-3xl mx-auto">
+          Hi! My name is Randy and I'm currently a second-year student at the
+          University of British Columbia pursuing a Bachelor's degree in
+          Engineering. I also enjoy hackathons and run track and field.
+        </p>
+      </DummyContent>
+    ),
+  },
+  {
+    category: "Skills",
+    title: "Languages",
+    src: "/images/carousel3.png", // Placeholder image, replace with your desired image
+    content: (
+      <DummyContent>
+        <div className="flex flex-wrap justify-center">
+          <Skill imageURL="/images/java.png" text="Java" />
+          <Skill imageURL="/images/python.png" text="Python" />
+          <Skill imageURL="/images/cpp.png" text="C++" />
+          <Skill imageURL="/images/js.png" text="JavaScript" />
+          <Skill imageURL="/images/ts.png" text="TypeScript" />
+          <Skill imageURL="/images/SQL.png" text="SQL" />
+          <Skill imageURL="/images/html.png" text="HTML5" />
+          <Skill imageURL="/images/css.png" text="CSS3" />
+        </div>
+      </DummyContent>
+    ),
+  },
+  {
+    category: "Skills",
     title: "Frameworks and Tools",
-    id: "frameworks-tools",
+    src: "/images/carousel4.png", // Placeholder image, replace with your desired image
     content: (
-      <div className="flex flex-wrap justify-center">
-        <Skill imageURL="/images/react.png" text="React" />
-        <Skill imageURL="/images/node.png" text="Node" />
-        <Skill imageURL="/images/next.png" text="Next.js" />
-        <Skill imageURL="/images/tailwind.png" text="Tailwind CSS" />
-        <Skill imageURL="/images/git.png" text="Git" />
-        <Skill imageURL="/images/github.png" text="GitHub" />
-        <Skill imageURL="/images/aws.png" text="AWS" />
-        <Skill imageURL="/images/mongoDB.png" text="MongoDB" />
-        <Skill imageURL="/images/figma.png" text="Figma" />
-        <Skill imageURL="/images/blender.png" text="Blender" />
-        <Skill imageURL="/images/web3.png" text="Web3" />
-        <Skill imageURL="/images/Fusion360.png" text="Fusion360" />
-      </div>
+      <DummyContent>
+        <div className="flex flex-wrap justify-center">
+          <Skill imageURL="/images/react.png" text="React" />
+          <Skill imageURL="/images/node.png" text="Node" />
+          <Skill imageURL="/images/next.png" text="Next.js" />
+          <Skill imageURL="/images/tailwind.png" text="Tailwind CSS" />
+          <Skill imageURL="/images/git.png" text="Git" />
+          <Skill imageURL="/images/github.png" text="GitHub" />
+          <Skill imageURL="/images/aws.png" text="AWS" />
+          <Skill imageURL="/images/mongoDB.png" text="MongoDB" />
+          <Skill imageURL="/images/figma.png" text="Figma" />
+          <Skill imageURL="/images/blender.png" text="Blender" />
+          <Skill imageURL="/images/web3.png" text="Web3" />
+          <Skill imageURL="/images/Fusion360.png" text="Fusion360" />
+        </div>
+      </DummyContent>
     ),
   },
   {
+    category: "Background",
     title: "Education",
-    id: "education",
+    src: "/images/carousel7.png", // Placeholder image, replace with your desired image
     content: (
-      <div className="text-left">
-        <p className="text-[white] text-xl font-bold">
-          University of British Columbia
-        </p>
-        <p className="text-[white] text-base">
-          Bachelor&apos;s of Applied Science in Electrical Engineering
-        </p>
-        <p className="text-[white] text-base">
-          Vancouver, BC, Canada · Sept 2023 - Apr 2027 (Expected)
-        </p>
-      </div>
-    )
+      <DummyContent>
+        <div className="text-left">
+          <p className="text-neutral-600 dark:text-neutral-50 text-base md:text-xl font-sans font-extrabold">
+            University of British Columbia
+          </p>
+          <p className="text-neutral-600 dark:text-neutral-400 text-base md:text-xl font-sans">
+            Bachelor&apos;s of Applied Science in Electrical Engineering
+          </p>
+          <p className="text-neutral-600 dark:text-neutral-400 text-base md:text-xl font-sans">
+            Vancouver, BC, Canada · Sept 2023 - Apr 2027 (Expected)
+          </p>
+        </div>
+      </DummyContent>
+    ),
   },
 ];
 
 const About: React.FC = () => {
-  const [tab, setTab] = useState<string>("skills");
-  const [isPending, startTransition] = useTransition();
-
-  const handleTabChange = (id: string) => {
-    startTransition(() => {
-      setTab(id);
-    });
-  };
+  const cards = data.map((card, index) => (
+    <Card key={card.src} card={card} index={index} />
+  ));
 
   return (
-    <section className="text-white" id="about">
-      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-        <div className="flex flex-col min-w-[300px] mx-auto">
-          <img
-            src="/images/myself.png"
-            alt="image of myself"
-            className="rounded-full w-[300px] h-[300px]"
-          />
-          <div className="flex mx-auto mt-5">
-            <Media />
-          </div>
-        </div>
-        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
-          <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
-          <p className="text-base lg:text-lg">
-            Hi! My name is Randy and I'm currently a second-year student at the University of British Columbia pursuing a Bachelor's degree in Engineering. I also enjoy hackathons and run track and field.
-          </p>
-          <div className="flex flex-row justify-start mt-8">
-            <TabButton selectTab={() => handleTabChange("skills")} active={tab === "skills"}>
-              Skills
-            </TabButton>
-            <TabButton selectTab={() => handleTabChange("frameworks-tools")} active={tab === "frameworks-tools"}>
-              Frameworks and Tools
-            </TabButton>
-            <TabButton selectTab={() => handleTabChange("education")} active={tab === "education"}>
-              Education
-            </TabButton>
-          </div>
-          <div className="mt-8">
-            {TAB_DATA.find((t) => t.id === tab)?.content}
-          </div>
-        </div>
-      </div>
-    </section>
+    <div className="w-full h-full py-20">
+      <h2 className="max-w-7xl pl-4 mx-auto text-xl md:text-5xl font-bold text-neutral-800 dark:text-neutral-200 font-sans">
+        Get to know me.
+      </h2>
+      <Carousel items={cards} />
+    </div>
   );
 };
 
