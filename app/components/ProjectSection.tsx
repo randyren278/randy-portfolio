406e37cca4209b3364d94a0e6174cc70b4d5fe74
randyren278 <randyren278@gmail.com>
2024-07-29 11:08:50 -0600

    edited animation sequence

--- app/components/ProjectSection.tsx
+++ app/components/ProjectSection.tsx
@@ -7,7 +7,8 @@ const projectsData = [
   {
     id: 1,
     title: "Weather App",
-    description: "A weather app with an intuitive and modern design, featuring real-time updates, 5-day forecast, and much more.",
+    description:
+      "A weather app with an intuitive and modern design, featuring real-time updates, 5-day forecast, and much more.",
     image: "/images/1.png",
     gitUrl: "https://github.com/randyren278/weather-app.git",
     previewUrl: "https://randy-weather-app.vercel.app/",
@@ -15,7 +16,8 @@ const projectsData = [
   {
     id: 2,
     title: "Wordle App",
-    description: "A popular word guessing game with a built-in solving algorithm.",
+    description:
+      "A popular word guessing game with a built-in solving algorithm.",
     image: "/images/2.png",
     gitUrl: "https://github.com/randyren278/Wordle.git",
     previewUrl: "https://randy-eldrow.vercel.app/",
@@ -23,21 +25,24 @@ const projectsData = [
   {
     id: 3,
     title: "ComputerVision-App",
-    description: "The application captures images from a webcam, detects faces, and recognizes them from a database.",
+    description:
+      "The application captures images from a webcam, detects faces, and recognizes them from a database.",
     image: "/images/3.png",
     gitUrl: "https://github.com/randyren278/computervision.git",
   },
   {
     id: 4,
     title: "3D-Modeller",
-    description: "This 3D modeller program allows you to create, manipulate, and view 3D objects in a virtual environment.",
+    description:
+      "This 3D modeller program allows you to create, manipulate, and view 3D objects in a virtual environment.",
     image: "/images/4.png",
     gitUrl: "https://github.com/randyren278/3D-Modeller.git",
   },
   {
     id: 5,
     title: "Portfolio",
-    description: "This application is a personal portfolio website, showcasing skills, projects, and background. This full-stack application features an interactive and visually appealing design.",
+    description:
+      "This application is a personal portfolio website, showcasing skills, projects, and background. This full-stack application features an interactive and visually appealing design.",
     image: "/images/5.png",
     gitUrl: "https://github.com/randyren278/randy-portfolio.git",
     previewUrl: "https://randyren.vercel.app/",
@@ -45,7 +50,8 @@ const projectsData = [
   {
     id: 6,
     title: "Credit Risk Analysis System",
-    description: "This application is designed to analyze the credit risk of customers. This system leverages a database to store customer data, credit histories, and analysis results.",
+    description:
+      "This application is designed to analyze the credit risk of customers. This system leverages a database to store customer data, credit histories, and analysis results.",
     image: "/images/6.png",
     gitUrl: "https://github.com/randyren278/CreditRiskAnalysis.git",
   },
@@ -55,19 +61,25 @@ const ProjectsSection: React.FC = () => {
   const ref = useRef(null);
   const isInView = useInView(ref, { once: true });
   const [revealedIndex, setRevealedIndex] = useState<number>(-1);
+  const [firstTrigger, setFirstTrigger] = useState<boolean>(true);
 
   useEffect(() => {
-    let currentIndex = -1;
-    const interval = setInterval(() => {
-      setRevealedIndex(currentIndex);
-      currentIndex += 1;
-      if (currentIndex > 5) {
-        currentIndex = -1; // Reset to loop
-      }
-    }, 2500); 
-
-    return () => clearInterval(interval);
-  }, [isInView]);
+    if (isInView) {
+      const specialIndexes = [ 0, 1, 2, 3, 4, 5, 6,7];
+      let currentIndex = -1;
+      const interval = setInterval(() => {
+        setRevealedIndex(currentIndex);
+        currentIndex += 1;
+        if (currentIndex >= projectsData.length) {
+          currentIndex = -1; // Reset to loop
+          setFirstTrigger(false); // Ensure subsequent loops have the delay
+        }
+      }, (currentIndex === -1 && firstTrigger) ? 2000 : specialIndexes.includes(currentIndex) && firstTrigger ? 2500 : 2500);
+  
+      return () => clearInterval(interval);
+    }
+  }, [isInView, firstTrigger]);
+  
 
   const cardVariants = {
     initial: { y: 50, opacity: 0 },
